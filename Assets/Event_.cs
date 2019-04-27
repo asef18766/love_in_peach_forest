a3f32d8e54b53ae446e4a169b9c49f091c62faec
BuildTools <unconfigured@null.spigotmc.org>
2019-04-27 18:05:05 +0800

    no message

---
+++ Assets/Event_.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("C# Script")]
+public class Event_ : ScriptableObject {
+
+    public string[] dialogs;
+    public float mood;
+    public Item_ take;
+    public Item_ send;
+    public float reqMood;
+    public int day;
+
+    public void accept(){
+
+    }
+}
---
+++ Assets/Item_.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_ : MonoBehaviour {
+
+    public string n;//the name of the item
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
