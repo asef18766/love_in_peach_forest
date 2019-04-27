37743838037bf682512169821c933657a8c0aea8
asef18766 <asef18766@gmail.com>
2019-04-27 20:41:19 +0800

    na

--- Assets/Event_.cs
+++ Assets/Event_.cs
@@ -22,7 +22,7 @@ public class Event_ : ScriptableObject
     public void Dialog()
     {
         canvas.Dialogs(done ? dialogs : rejectDialogs);
-        if (!canvas.show)
+        if (!Canvas_.show)
         {
             //remove event
         }
