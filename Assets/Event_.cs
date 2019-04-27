99694a2e281db7a8998ef28129f0bb39d68f85c5
BuildTools <unconfigured@null.spigotmc.org>
2019-04-27 20:13:17 +0800

    no message

--- Assets/Event_.cs
+++ Assets/Event_.cs
@@ -3,16 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [AddComponentMenu("C# Script")]
-public class Event_ : ScriptableObject {
+public class Event_ : ScriptableObject
+{
 
     public string[] dialogs;
+    public string[] rejectDialogs;
     public float mood;
     public Item_ take;
     public Item_ send;
     public float reqMood;
     public int day;
+    public int askIndex;//set to 0 if you don't want to ask
+    public Entity_ player;
+    public Canvas_ canvas;
+    int index = 0;
+    bool done = true;
 
-    public void accept(){
+    public void Dialog()
+    {
+        canvas.Dialogs(done ? dialogs : rejectDialogs);
+        if (!canvas.show)
+        {
+            //remove event
+        }
 
+        index++;
+        if (index == askIndex) done = Accept();
+    }
+
+    public bool Accept()
+    {
+        if (take == null || player.inventory.Contains(take))
+        {
+            if (take != null) player.inventory.Remove(take);
+            player.prefer += mood;
+            if (send != null) player.inventory.Add(send);
+            return true;
+        }
+
+        return false;
+    }
+
+    public void Awake()
+    {
+        string[] s = new string[askIndex + rejectDialogs.Length];
+        rejectDialogs.CopyTo(s, askIndex);
+        rejectDialogs = s;
     }
 }
